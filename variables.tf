b973c2a19429ae6b767d1e033af05120ccb19e15
Thibautdbr33 <thibautdijouxbrezot@gmail.com>
2025-06-05 16:27:23 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "subscription" {
   description = "ID de l’abonnement Azure"
   type        = string
 }
+
+variable "env" {
+  description = "Environnement de déploiement (dev ou prod)"
+  type        = string
+}
