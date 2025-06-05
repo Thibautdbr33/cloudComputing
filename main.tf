37fff9b962e4a60322bf530b10d6e4c0bc03e828
Thibaut Dijoux Brezot <thibdij974@gmail.com>
2025-06-05 12:31:06 +0200

    Clean history: remove access_key from history

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.77.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "iacstorage974"             
+    container_name       = "tfstate"                    
+    key                  = "vm_deploy.tfstate"          
+  }
+}
+
+provider "azurerm" {
+  subscription_id            = var.subscription
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "rg-iac-thibaut"
+  location = "francecentral"
+}
+
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "vnet-iac-thibaut"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+}
+
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "subnet-iac-thibaut"
+  resource_group_name  = azurerm_resource_group.my_resource_group.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_virtual_machine" "my_vm" {
+  name                  = "vm-iac-thibaut"
+  resource_group_name   = azurerm_resource_group.my_resource_group.name
+  location              = azurerm_resource_group.my_resource_group.location
+  availability_set_id   = azurerm_availability_set.my_availability_set.id
+  network_interface_ids = [azurerm_network_interface.my_nic.id]
+
+  vm_size                         = "Standard_B1s"
+  delete_os_disk_on_termination  = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "20.04.202209200"
+  }
+
+  storage_os_disk {
+    name              = "vm-iac-thibaut-OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = "vm-iac-thibaut"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+resource "azurerm_availability_set" "my_availability_set" {
+  name                = "avset-iac-thibaut"
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+  location            = azurerm_resource_group.my_resource_group.location
+}
+
+resource "azurerm_network_interface" "my_nic" {
+  name                = "nic-iac-thibaut"
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+  depends_on = [azurerm_subnet.my_subnet]
+
+  ip_configuration {
+    name                          = "nicconf-iac-thibaut"
+    subnet_id                     = azurerm_subnet.my_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
